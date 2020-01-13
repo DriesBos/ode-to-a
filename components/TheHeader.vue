4265f2d0a91184959479dadb46e50b782342e16d
Dries Bos <info@driesbos.com>
2020-01-13 16:46:07 +0000

    header styling

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,9 +2,10 @@
   <header>
     <nav>
       <ul>
-        <nuxt-link to="/" tag="li">Home</nuxt-link>
-        <nuxt-link to="/blog" tag="li">Blog</nuxt-link>
-        <nuxt-link to="/about" tag="li">About</nuxt-link>
+        <nuxt-link to="/" tag="li">A —</nuxt-link>
+        <nuxt-link to="/brands" tag="li">For Brands</nuxt-link>
+        <nuxt-link to="/people" tag="li">For People</nuxt-link>
+        <nuxt-link to="/art" tag="li">For Art</nuxt-link>
       </ul>
     </nav>
   </header>
