8f7a2537e26f6babe41c1279d0228d0f1be995a8
Dries Bos <info@driesbos.com>
2020-01-21 13:52:44 +0000

    added listItem component

---
+++ components/Listitem.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- prettier-ignore -->
+  <section v-editable="blok" class="listitem" :class="{ smallsize: blok.small_size, fullscreen: blok.fullscreen }">
+    <div class="listitem-Container">
+    <p>{{ blok.title }}</p>
+    <p>{{ blok.content }}</p> 
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: Object
+  },
+  mounted() {
+    // console.log(this.blok)
+  }
+}
+</script>
--- plugins/components.js
+++ plugins/components.js
@@ -9,6 +9,7 @@ import Textitem from "~/components/Textitem.vue"
 import FooterForm from "~/components/FooterForm.vue"
 import GridItem from "~/components/GridItem.vue"
 import ImageListItem from "~/components/ImageListItem.vue"
+import Listitem from "~/components/Listitem.vue"
 
 Vue.component("brandslist", BrandsListItem)
 Vue.component("blok-landingitem", Landingitem)
@@ -19,3 +20,4 @@ Vue.component("blok-textitem", Textitem)
 Vue.component("blok-footerform", FooterForm)
 Vue.component("blok-griditem", GridItem)
 Vue.component("blok-imagelistitem", ImageListItem)
+Vue.component("blok-listitem", Listitem)
