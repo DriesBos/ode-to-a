3f8d09662475e713a6a8002396d693b88b1426ed
Dries Bos <info@driesbos.com>
2020-02-02 17:00:10 +0000

    svg to listItem

--- components/Listitem.vue
+++ components/Listitem.vue
@@ -5,6 +5,19 @@
     class="listitem"
     :class="{ smallsize: blok.small_type, fullscreen: blok.fullscreen }"
   >
+    <svg viewBox="0 0 220 356.96">
+      <g data-name="Laag 2">
+        <!-- prettier-ignore -->
+        <path
+          d="M217.25,356.21v-213c0-12-7-16.5-18-11L.75,230.71v-120L219.25,1.21v355Z"
+          fill="none"
+          stroke
+          stroke-miterlimit="10"
+          stroke-width="1.5"
+          data-name="Laag 1"
+        />
+      </g>
+    </svg>
     <div class="listitem-Container">
       <h1>{{ blok.title }}</h1>
       <p>{{ blok.content }}</p>
