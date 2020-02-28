a5648ddaea69208d85f651871adf9e12468d6ebb
Dries Bos <info@driesbos.com>
2020-02-28 17:09:54 +0100

    fix: contact form to netlify

--- components/FooterForm.vue
+++ components/FooterForm.vue
@@ -6,12 +6,13 @@
       <div class="markdown">
         <p>Let’s talk! Be bold, don’t be shy. We would love to have a moment with you.</p>
       </div>
-      <form action>
-        <input type="text" placeholder="NAME" />
-        <input type="email" placeholder="EMAIL" />
-        <input type="text" placeholder="CITY" />
-        <input type="text" placeholder="FIRE AWAY" />
-        <button>SEND
+      <form name="contact" method="post" action="/succes" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact" />
+        <input type="text" name="name" placeholder="NAME" />
+        <input type="email" name="email" placeholder="EMAIL" />
+        <input type="text" name="city" placeholder="CITY" />
+        <input type="text" name="message" placeholder="FIRE AWAY" />
+        <button type="submit">SEND
           <svg viewBox="0 0 46.65 37.7">
             <g data-name="Laag 2">
               <path d="M27.8,37.7H17.55l15.2-15H0V15H32.75L17.55,0H27.8L46.65,18.85Z" data-name="Laag 1"/>
