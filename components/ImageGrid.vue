4bb6f76d95086964e67fc48708170663264106d4
Dries Bos <info@driesbos.com>
2020-02-28 16:14:01 +0100

    feat: init imageGrid intersectObserver

--- components/ImageGrid.vue
+++ components/ImageGrid.vue
@@ -2,7 +2,7 @@
   <!-- prettier-ignore -->
   <section v-editable="blok" class="imageGrid">
     <ul>
-      <li v-for="image in blok.image" :key="image.name">
+      <li  v-for="image in blok.image" :key="image.name" class="imageGrid-Item">
         <img :src="image.filename" />
       </li>
     </ul>
@@ -13,6 +13,33 @@
 export default {
   props: {
     blok: Object
+  },
+  data() {
+    return {
+      observer: null
+    }
+  },
+  mounted() {
+    const targets = document.querySelectorAll(".imageGrid-Item")
+    const lazyFilter = target => {
+      this.observer = new IntersectionObserver(
+        entries => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              entry.target.classList.remove("filter")
+            } else {
+              entry.target.classList.add("filter")
+            }
+          })
+        },
+        { threshold: 0.7 }
+      )
+      this.observer.observe(target)
+    }
+    targets.forEach(lazyFilter)
+  },
+  destroyed() {
+    this.observer.disconnect()
   }
 }
 </script>
