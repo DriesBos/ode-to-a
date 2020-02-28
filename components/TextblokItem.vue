04ec98ac73c82a9ecf5a8ac66302438384544772
Dries Bos <info@driesbos.com>
2020-02-28 13:39:23 +0100

    feat: added graphic to textItem

--- components/TextblokItem.vue
+++ components/TextblokItem.vue
@@ -5,6 +5,9 @@
     class="textBlok intersectionObserver"
     :class="{ fullscreen: blok.fullscreen }"
   >
+    <div class="graphic" :class="blok.graphic_alignment">
+      <img :src="blok.graphic" />
+    </div>
     <markdown v-if="blok.text" class="textBlok-Main" :input="blok.text" />
     <markdown v-if="blok.fade_in_text" class="textBlok-FadeIn" :input="blok.fade_in_text" />
   </section>
