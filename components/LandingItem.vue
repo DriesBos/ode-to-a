1e2d6c904a50364d15f9994b55cb137fa197e366
Dries Bos <info@driesbos.com>
2020-03-02 16:47:59 +0100

    style: landingItem filter

--- components/LandingItem.vue
+++ components/LandingItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <section v-editable="blok" class="landingItem">
+  <section v-editable="blok" class="landingItem" :class="{ filter: filtered }">
     <img v-if="blok.image" :src="blok.image" />
     <div class="landingItem-Text">
       <h1 v-if="blok.title">{{ blok.title }}</h1>
@@ -11,6 +11,32 @@
 export default {
   props: {
     blok: Object
+  },
+  data() {
+    return {
+      filtered: true
+    }
+  },
+  mounted() {
+    window.addEventListener("scroll", this.applyFilter)
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.applyFilter)
+  },
+  methods: {
+    applyFilter() {
+      const currentScrollPosition =
+        window.pageYOffset || document.documentElement.scrollTop
+      const el = document.querySelector(".landingItem")
+      if (
+        currentScrollPosition > window.innerHeight * 0.1 &&
+        currentScrollPosition < window.innerHeight * 0.6
+      ) {
+        el.classList.remove("filter")
+      } else {
+        el.classList.add("filter")
+      }
+    }
   }
 }
 </script>
