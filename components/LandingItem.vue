dd5d7e0fff5686771900d914642ed0a6fb133a78
Dries Bos <info@driesbos.com>
2020-03-02 18:07:36 +0100

    fix

--- components/LandingItem.vue
+++ components/LandingItem.vue
@@ -1,6 +1,12 @@
 <template>
   <section v-editable="blok" class="landingItem" :class="{ filter: filtered }">
-    <img v-if="blok.image" :src="blok.image" />
+    <!-- prettier-ignore -->
+    <img
+      v-if="blok.image"
+      :srcset="`${transformImage(blok.image, '2880x0')} 2880w, ${transformImage(blok.image, '2560x0')} 2560w, ${transformImage(blok.image, '1920x0')} 1920w, ${transformImage(blok.image, '1680x0')} 1680w, ${transformImage(blok.image, '1370x0')} 1370w, ${transformImage(blok.image, '900x0')} 900w`"
+      sizes="100vw"
+      :data-src="`${transformImage(blok.image, '1440')}`"
+    />
     <div class="landingItem-Text">
       <h1 v-if="blok.title">{{ blok.title }}</h1>
     </div>
@@ -36,6 +42,13 @@ export default {
       } else {
         el.classList.add("filter")
       }
+    },
+    transformImage(image, option) {
+      if (!image) return ""
+      if (!option) return ""
+      let imageService = "//img2.storyblok.com/"
+      let path = image.replace("//a.storyblok.com", "")
+      return imageService + option + "/filters:quality(10)" + path
     }
   }
 }
