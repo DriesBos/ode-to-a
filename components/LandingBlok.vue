66777ea53f4c8668d7f46f9d5b252897b6917362
Dries Bos <info@driesbos.com>
2020-03-04 23:14:12 +0100

    cleanup

--- components/LandingBlok.vue
+++ components/LandingBlok.vue
@@ -2,12 +2,12 @@
   <section v-editable="blok" class="landingItem" :class="{ filter: filtered }">
     <!-- prettier-ignore -->
     <div class="vueLazy" v-lazy-container="{ selector: 'img' }">
-    <img
-      v-if="blok.image"
-      :srcset="`${transformImage(blok.image, '2880x0')} 2880w, ${transformImage(blok.image, '2560x0')} 2560w, ${transformImage(blok.image, '1920x0')} 1920w, ${transformImage(blok.image, '1680x0')} 1680w, ${transformImage(blok.image, '1370x0')} 1370w, ${transformImage(blok.image, '900x0')} 900w`"
-      sizes="100vw"
-      :data-src="`${transformImage(blok.image, '1440')}`"
-    />
+      <img
+        v-if="blok.image"
+        :srcset="`${transformImage(blok.image, '2880x0')} 2880w, ${transformImage(blok.image, '2560x0')} 2560w, ${transformImage(blok.image, '1920x0')} 1920w, ${transformImage(blok.image, '1680x0')} 1680w, ${transformImage(blok.image, '1370x0')} 1370w, ${transformImage(blok.image, '900x0')} 900w`"
+        sizes="100vw"
+        :data-src="`${transformImage(blok.image, '1440')}`"
+      />
     </div>
     <div class="landingItem-Text">
       <h1 v-if="blok.title">{{ blok.title }}</h1>
