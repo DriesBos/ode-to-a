d44f875b95c0a37c3878d6a7ccbf1d64e607103a
Dries Bos <info@driesbos.com>
2020-03-21 19:21:14 +0100

    style: mre info update

--- components/TheMoreInfo.vue
+++ components/TheMoreInfo.vue
@@ -5,11 +5,13 @@
       <h3>Free to stop by at our office, we would love to meet!</h3>
     </div>
     <div class="theMoreInfo-Content">
-      <h1>
-        ode to a
-        <br />Herengracht 216
+      <h1>Ode to A</h1>
+      <a
+        href="https://www.google.com/maps/place/Ode+to+A/@52.3726791,4.884561,17z/data=!3m1!4b1!4m5!3m4!1s0x47c609e5a21203f5:0xd51aedf496f92576!8m2!3d52.3726791!4d4.8867497"
+      >
+        Herengracht 216
         <br />1016BS Amsterdam
-      </h1>
+      </a>
     </div>
     <div class="theMoreInfo-Close">
       <h5 @click="onClick()">CLOSE</h5>
