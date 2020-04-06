6f14e6a20dc9d1e005ad71e0cc6c4eaf2a3736e9
Dries Bos <info@driesbos.com>
2020-04-06 10:53:01 +0200

    fix: graphic el overlay

--- components/GraphicBlok.vue
+++ components/GraphicBlok.vue
@@ -14,3 +14,38 @@ export default {
   }
 }
 </script>
+
+<style lang="sass">
+@import '~assets/styles/variables'
+
+.landinggraphicitem
+  position: absolute
+  display: flex
+  justify-content: flex-end
+  top: 0
+  left: 0
+  width: 100%
+  height: 100vh
+  padding-left: var(--side-spacing)
+  padding-right: var(--side-spacing)
+  padding-top: var(--topside-spacing)
+  padding-bottom: var(--topside-spacing)
+  z-index: +1
+  &-Container
+    display: flex
+    align-items: center
+    height: 100%
+    width: 100%
+    img
+      height: 82vmin
+      width: 82vmin
+      max-width: 100%
+      max-height: 100%
+    &.left
+      justify-content: flex-start
+    &.center
+      img
+        width: 100%
+    &.right
+      justify-content: flex-end
+</style>
