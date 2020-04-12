5a3236f588362cf7a05279fe6e4fcdfdac5f5f98
Dries Bos <info@driesbos.com>
2020-04-12 23:18:35 +0200

    footer content update

--- components/TheMoreInfo.vue
+++ components/TheMoreInfo.vue
@@ -5,13 +5,9 @@
       <h3>Free to stop by at our office, we would love to meet!</h3>
     </div>
     <div class="theMoreInfo-Content">
-      <h1>Ode to A</h1>
-      <a
-        href="https://www.google.com/maps/place/Ode+to+A/@52.3726791,4.884561,17z/data=!3m1!4b1!4m5!3m4!1s0x47c609e5a21203f5:0xd51aedf496f92576!8m2!3d52.3726791!4d4.8867497"
-      >
-        Herengracht 216
-        <br />1016BS Amsterdam
-      </a>
+      <a href="https://goo.gl/maps/QnLbb1psM5Gdyw3e6">MEET US</a>
+      <a href="tel:0031630108213">CALL US</a>
+      <a href="https://www.instagram.com/odetoa__/">FOLLOW US</a>
     </div>
     <div class="theMoreInfo-Close">
       <h5 @click="onClick()">CLOSE</h5>
