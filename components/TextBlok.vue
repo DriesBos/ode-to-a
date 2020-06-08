d048390f2466d74c3b6723544ffd981823ea0712
Dries Bos <info@driesbos.com>
2020-06-08 20:51:31 +0200

    Skew graphics

--- components/TextBlok.vue
+++ components/TextBlok.vue
@@ -6,7 +6,7 @@
     :class="{ fullscreen: blok.fullscreen, textCenter: blok.left_alignment, capslock: blok.capslock }"
   >
     <div class="graphic" :class="blok.graphic_alignment">
-      <img v-if="blok.graphic" :src="blok.graphic">
+      <img v-if="blok.graphic" :src="blok.graphic" class="skewGraphic">
     </div>
     <markdown v-if="blok.text" class="textBlok-Item textBlok-Main" :input="blok.text" />
     <markdown
@@ -18,11 +18,43 @@
 </template>
 
 <script>
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
+gsap.registerPlugin(ScrollTrigger)
+
 export default {
   props: {
     blok: Object
   },
-  mounted() {}
+  mounted() {
+    this.skewGraphic(10)
+  },
+  methods: {
+    skewGraphic(range) {
+      let proxy = { skew: 0 },
+        skewSetter = gsap.quickSetter(".skewGraphic", "skewY", "deg"), // fast
+        clamp = gsap.utils.clamp(`-${range}`, range) // don't let the skew go beyond 20 degrees.
+      ScrollTrigger.create({
+        onUpdate: self => {
+          let skew = clamp(self.getVelocity() / -300)
+          // only do something if the skew is MORE severe. Remember, we're always tweening back to 0, so if the user slows their scrolling quickly, it's more natural to just let the tween handle that smoothly rather than jumping to the smaller skew.
+          if (Math.abs(skew) > Math.abs(proxy.skew)) {
+            proxy.skew = skew
+            gsap.to(proxy, {
+              skew: 0,
+              duration: 2,
+              ease: "power3",
+              overwrite: true,
+              onUpdate: () => skewSetter(proxy.skew)
+            })
+          }
+        }
+      })
+      // make the right edge "stick" to the scroll bar. force3D: true improves performance
+      gsap.set(".skewElem", { transformOrigin: "right center", force3D: true })
+    }
+  }
 }
 </script>
 
