041474064346df6024665ca66d50f6753bff84a1
Dries Bos <info@driesbos.com>
2020-06-08 18:57:57 +0200

    Updated: image delivery ProjectItems

--- components/ProjectsItem.vue
+++ components/ProjectsItem.vue
@@ -10,28 +10,36 @@
     <a v-if="blok.hyperlink.linktype === 'url'" :href="blok.hyperlink.cached_url" class="content">{{ blok.title }}</a>
 
     <h3 class="dash">—&nbsp;</h3>
+
+    <!-- prettier-ignore -->
     <div v-lazy-container="{ selector: 'img' }" class="vueLazy projectList-Single_Image">
       <img
         v-if="blok.image"
         :srcset="
-          `${transformImage(blok.image, '2880x0')} 2880w, ${transformImage(
+          `${transformImage(
             blok.image,
-            '2560x0'
+            '2880x0/filters:format(jpg):quality(65)'
+          )} 2880w, ${transformImage(
+            blok.image,
+            '2560x0/filters:format(jpg):quality(65)'
           )} 2560w, ${transformImage(
             blok.image,
-            '1920x0'
+            '1920x0/filters:format(jpg):quality(65)'
           )} 1920w, ${transformImage(
             blok.image,
-            '1680x0'
+            '1680x0/filters:format(jpg):quality(65)'
           )} 1680w, ${transformImage(
             blok.image,
-            '1370x0'
-          )} 1370w, ${transformImage(blok.image, '900x0')} 900w`
-        "
-        sizes="100vw"
-        :data-src="`${transformImage(blok.image, '1440')}`"
+            '1370x0/filters:format(jpg):quality(65)'
+          )} 1370w, ${transformImage(
+            blok.image,
+            '900x0/filters:format(jpg):quality(65)'
+          )} 900w`
+        " sizes="100vw"
+        :data-src="`${transformImage(blok.image, '1600x0/filters:format(jpg):quality(65)')}`"
       >
     </div>
+
   </span>
 </template>
 
