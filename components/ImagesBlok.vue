65c72cd913586b4bc8adff17759da8f280d73ac3
Dries Bos <info@driesbos.com>
2020-06-08 15:45:06 +0200

    added image skew

--- components/ImagesBlok.vue
+++ components/ImagesBlok.vue
@@ -2,9 +2,9 @@
   <!-- prettier-ignore -->
   <section :id="blok._uid" v-editable="blok" class="imageGrid">
     <ul>
-      <li v-for="image in blok.image" :key="image.filename" class="imageGrid-Item">
+      <li v-for="image in blok.image" :key="image.filename" class="imageGrid-Item skewElem">
         <div class="imageGrid-Item_Placeholder">
-          <img :src="image.filename">
+          <img :src="image.filename" />
         </div>
       </li>
     </ul>
@@ -30,6 +30,7 @@ export default {
   },
   mounted() {
     this.parallax()
+    this.skewImage(3)
     this.IntersectionObserverMixin(
       ".imageGrid-Item_Placeholder",
       "filter",
@@ -101,6 +102,29 @@ export default {
         this.observerName.observe(element)
       }
       elements.forEach(observerLoop)
+    },
+    skewImage(range) {
+      let proxy = { skew: 0 },
+        skewSetter = gsap.quickSetter(".skewElem", "skewY", "deg"), // fast
+        clamp = gsap.utils.clamp(`-${range}`, range) // don't let the skew go beyond 20 degrees.
+      ScrollTrigger.create({
+        onUpdate: self => {
+          let skew = clamp(self.getVelocity() / -300)
+          // only do something if the skew is MORE severe. Remember, we're always tweening back to 0, so if the user slows their scrolling quickly, it's more natural to just let the tween handle that smoothly rather than jumping to the smaller skew.
+          if (Math.abs(skew) > Math.abs(proxy.skew)) {
+            proxy.skew = skew
+            gsap.to(proxy, {
+              skew: 0,
+              duration: 0.8,
+              ease: "power3",
+              overwrite: true,
+              onUpdate: () => skewSetter(proxy.skew)
+            })
+          }
+        }
+      })
+      // make the right edge "stick" to the scroll bar. force3D: true improves performance
+      gsap.set(".skewElem", { transformOrigin: "right center", force3D: true })
     }
   }
 }
